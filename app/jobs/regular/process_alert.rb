discourse/discourse-prometheus-alert-receiver

commit b59215173fdff88f5157d96955d80ca0218ab7e9

Log when we fail to assign someone on OpsGenue rotation.

app/jobs/regular/process_alert.rb
@@ -165,6 +165,10 @@ module Jobs
         assignee ||= begin
           if user_on_rotation = OpsgenieSchedule.users_on_rotation.sample
             assignee = User.find_by_username_or_email(user_on_rotation)
+
+            if !assignee
+              Rails.logger.warn("Failed to assign alert topic to '#{user_on_rotation}'")
+            end
           else
             random_group_member(receiver)
           end

commit ed21dabf094055d3dd2c0af200dae187e5022995

FIX: Assignment broke in https://github.com/discourse/discourse-prometheus-alert-receiver/commit/b59215173fdff88f5157d96955d80ca0218ab7e9.

app/jobs/regular/process_alert.rb
@@ -168,14 +168,18 @@ module Jobs
             assignee = User.find_by_username_or_email(user_on_rotation)
 
             if !assignee
-              Rails.logger.warn("Failed to assign alert topic to '#{user_on_rotation}'")
+              Rails.logger.warn(
+                "Failed to assign alert topic to '#{user_on_rotation}'"
+              )
             end
+
+            assignee
           else
             random_group_member(receiver)
           end
         end
 
-        unless assignee.nil?
+        if assignee
           TopicAssigner.new(t, Discourse.system_user).assign(assignee)
         end
       end
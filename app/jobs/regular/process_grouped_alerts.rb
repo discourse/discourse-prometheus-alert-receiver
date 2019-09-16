991333bec4a92a6bf49b3e044265672ee0fd3e3c
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2019-09-17 03:39:58 +1000

    FIX: Use top-level namespace for base classes (#6)

--- app/jobs/regular/process_grouped_alerts.rb
+++ app/jobs/regular/process_grouped_alerts.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Jobs
-  class ProcessGroupedAlerts < Jobs::Base
+  class ProcessGroupedAlerts < ::Jobs::Base
     sidekiq_options retry: false
 
     include AlertPostMixin

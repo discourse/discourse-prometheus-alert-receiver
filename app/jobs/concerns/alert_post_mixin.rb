da8e3694055e57932abadc5d526f042dda14d9ef
Sam Saffron <sam.saffron@gmail.com>
2021-10-29 12:06:29 +1100

    UI: Change high-priority tag to priority-high
    Our various styling using priority-high vs high-priority.

--- app/jobs/concerns/alert_post_mixin.rb
+++ app/jobs/concerns/alert_post_mixin.rb
@@ -4,7 +4,7 @@ module AlertPostMixin
   extend ActiveSupport::Concern
 
   FIRING_TAG = "firing".freeze
-  HIGH_PRIORITY_TAG = "high-priority".freeze
+  HIGH_PRIORITY_TAG = "priority-high".freeze
   NEXT_BUSINESS_DAY_SLA = "nbd".freeze
 
   MAX_BUMP_RATE = 5.minutes

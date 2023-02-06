c6a2918dc383b5160b47e6122bb56804c7f44c43
David Taylor <david@taylorhq.com>
2023-02-06 14:42:37 +0000

    Apply suggested whitespace changes
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>

--- assets/javascripts/discourse/components/alert-receiver/table.js
+++ assets/javascripts/discourse/components/alert-receiver/table.js
@@ -1,6 +1,7 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
+
 export default class AlertReceiverTable extends Component {
   @tracked collapsed;
 
--- assets/javascripts/discourse/components/alert-receiver/timestamp.js
+++ assets/javascripts/discourse/components/alert-receiver/timestamp.js
@@ -1,7 +1,6 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { applyLocalDates } from "discourse/lib/local-dates";
-
 import { inject as service } from "@ember/service";
 
 export default class AlertReceiverDate extends Component {

6f0b56e2775c6f817c5d3dea7f4efbd63af0c7c7
Jarek Radosz <jradosz@gmail.com>
2024-01-16 01:10:16 +0100

    DEV: Update linting

--- assets/javascripts/discourse/components/alert-receiver/table.js
+++ assets/javascripts/discourse/components/alert-receiver/table.js
@@ -1,6 +1,6 @@
 import Component from "@glimmer/component";
-import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 export default class AlertReceiverTable extends Component {
   @tracked collapsed;
--- assets/javascripts/discourse/components/alert-receiver/timestamp.js
+++ assets/javascripts/discourse/components/alert-receiver/timestamp.js
@@ -1,7 +1,7 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
-import { applyLocalDates } from "discourse/lib/local-dates";
 import { inject as service } from "@ember/service";
+import { applyLocalDates } from "discourse/lib/local-dates";
 
 export default class AlertReceiverDate extends Component {
   @service siteSettings;

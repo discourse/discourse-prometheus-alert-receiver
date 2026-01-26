057704a5ba33a340ee52c8699dbf2e3008b33cd7
Jarek Radosz <jarek@cvx.dev>
2026-01-26 20:31:59 +0100

    DEV: Update linting and add types

--- spec/integration/discourse_prometheus_alert_receiver/receiver_controller_spec.rb
+++ spec/integration/discourse_prometheus_alert_receiver/receiver_controller_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 RSpec.describe DiscoursePrometheusAlertReceiver::ReceiverController do
   let(:category) { Fabricate(:category) }
   let(:assignee_group) { Fabricate(:group) }
--- spec/model/topic_spec.rb
+++ spec/model/topic_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 describe Topic do
   fab!(:topic)
   fab!(:topic_2, :topic)
